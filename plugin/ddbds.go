4f17e88959362c39682d431c259a5f4a6d0c72e2
Vanssh-k <vanshkapoor2001@gmail.com>
2025-03-04 12:50:58 +0530

    Updated aws config

--- plugin/ddbds.go
+++ plugin/ddbds.go
@@ -44,40 +44,40 @@ func (p *DDBPlugin) DatastoreConfigParser() fsrepo.ConfigFromMap {
 
 		accessKey, _ := m["accessKey"].(string)
 		secretKey, _ := m["secretKey"].(string)
-		sessionToken, _ := m["sessionToken"].(string)
+		region, _ := m["region"].(string)
 
 		return &DDBConfig{
-			Table:        table,
-			AccessKey:    accessKey,
-			SecretKey:    secretKey,
-			SessionToken: sessionToken,
+			Table:     table,
+			AccessKey: accessKey,
+			SecretKey: secretKey,
+			Region:    region,
 		}, nil
 	}
 }
 
 type DDBConfig struct {
-	Table        string
-	AccessKey    string
-	SecretKey    string
-	SessionToken string
+	Table     string
+	AccessKey string
+	SecretKey string
+	Region    string
 }
 
 func (c *DDBConfig) DiskSpec() fsrepo.DiskSpec {
 	return fsrepo.DiskSpec{
-		"table":        c.Table,
-		"accessKey":    c.AccessKey,
-		"secretKey":    c.SecretKey,
-		"sessionToken": c.SessionToken,
+		"table":     c.Table,
+		"accessKey": c.AccessKey,
+		"secretKey": c.SecretKey,
+		"region": c.Region
 	}
 }
 
 func (c *DDBConfig) Create(path string) (repo.Datastore, error) {
 	awsConfig := &aws.Config{
-		Region: aws.String("us-east-1"),
+		Region: aws.String(c.Region),
 	}
 
 	if c.AccessKey != "" && c.SecretKey != "" {
-		awsConfig.Credentials = credentials.NewStaticCredentials(c.AccessKey, c.SecretKey, c.SessionToken)
+		awsConfig.Credentials = credentials.NewStaticCredentials(c.AccessKey, c.SecretKey)
 	}
 
 	sess, err := session.NewSession(awsConfig)
